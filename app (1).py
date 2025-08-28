af6348a0bd7a3c6b012b1a3668b93d99409037d1
PAzZInDu <pasindu.karunanayaka@pyxeda.ai>
2025-08-28 17:26:48 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,78 @@
+import streamlit as st
+import numpy as np
+import requests
+import base64
+import os
+from PIL import Image
+
+
+ENDPOINT_URL = 'https://askai.aiclub.world/27a37a06-93bd-4ec5-a56c-963adc53c0e7'
+
+#####functions#########
+def get_prediction(image_data, url):
+  r = requests.post(url, data=image_data)
+  response = r.json()['predicted_label']
+  print(response)
+  return response
+
+  
+
+#Building the website
+
+#title of the web page
+st.title("Butterfly Classifer")
+
+#setting the main picture
+st.image(
+    "https://t4.ftcdn.net/jpg/10/09/58/79/360_F_1009587933_xfLSLUHWaMJDnhvB6rJFtYZosRs0ObNr.jpg", 
+    caption = "Butterfly")
+
+#about the web app
+st.header("About the Web App")
+
+#details about the project
+with st.expander("Web App 🌐"):
+    st.subheader("Butterfly Image Predictions")
+    st.write("This web app is about.....................")
+
+#setting the tabs
+tab1, tab2 = st.tabs(['Image Upload 👁️', 'Camera Upload 📷'])
+
+#tab1
+with tab1:
+    #setting file uploader
+    #you can change the label name as your preference
+    image = st.file_uploader(label="Upload an image",accept_multiple_files=False, help="Upload an image to classify them")
+
+    if image:
+        #validating the image type
+        image_type = image.type.split("/")[-1]
+        if image_type not in ['jpg','jpeg','png','jfif']:
+            st.error("Invalid file type : {}".format(image.type), icon="🚨")
+        else:
+            #displaying the image
+            st.image(image, caption = "Uploaded Image")
+
+            #getting the predictions
+            with image:
+                payload = base64.b64encode(image.read())
+                response = get_prediction(payload, ENDPOINT_URL)
+                st.success(f"Class Label: {response}")
+
+
+with tab2:
+    #camera input
+    cam_image = st.camera_input("Please take a photo")
+
+    if cam_image:
+        #displaying the image
+        st.image(cam_image)
+
+        #getting the predictions
+        with cam_image:
+            payload = base64.b64encode(image.read())
+            response = get_prediction(payload, ENDPOINT_URL)
+            
+            #displaying the predicted label
+            st.success("Your Condition is **{}**".format(label))
+            

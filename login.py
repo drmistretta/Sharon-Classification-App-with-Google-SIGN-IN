ebe73c8af19465f42136d0d8114e641afcd2456e
Dr. Sharon Mistretta <7537846+drmistretta@users.noreply.github.com>
2025-09-16 11:37:20 -0400

    Update login.py
    Comments

--- login.py
+++ login.py
@@ -11,8 +11,9 @@ import authlib
 # IMAGE_ADDRESS is a variable linked to a string which is the URL of an image on the freepik website
 #
 IMAGE_ADDRESS = "https://img.freepik.com/free-photo/fantasy-landscape-with-butterfly_23-2151451739.jpg"
-
-
+#
+# The following if/else routine checks to see if the user is already logged into Google
+# The "true" condition is invoked when Streamlit detects that the user is NOT logged in:
 if not st.user.is_logged_in:
     st.title("Google Login App")
     st.image(IMAGE_ADDRESS)

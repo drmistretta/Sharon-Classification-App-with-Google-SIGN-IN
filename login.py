8d1f253776007e6082e85745320ce94f24f87fe4
Dr. Sharon Mistretta <7537846+drmistretta@users.noreply.github.com>
2025-09-16 11:46:22 -0400

    Update login.py
    Update login image

--- login.py
+++ login.py
@@ -10,7 +10,7 @@ import authlib
 # IMAGE_ADDRESS is a module-level (login.py), global variable that is shared across this file
 # IMAGE_ADDRESS is a variable linked to a string which is the URL of an image on the freepik website
 #
-IMAGE_ADDRESS = "https://img.freepik.com/free-photo/fantasy-landscape-with-butterfly_23-2151451739.jpg"
+IMAGE_ADDRESS = "https://images.unsplash.com/photo-1623615412998-c63b6d5fe9be?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bW9uYXJjaCUyMGJ1dHRlcmZseXxlbnwwfHwwfHx8MA%3D%3D"
 #
 # The following if/else routine checks to see if the user is already logged into Google
 # The "true" condition is invoked when Streamlit detects that the user is NOT logged in:
